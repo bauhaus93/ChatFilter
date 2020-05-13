7228c47c075429d4fd2a7b698d1b13210ac9d3e3
Jakob Fischer <JakobFischer93@gmail.com>
2020-05-13 18:53:57 +0200

    Added basic filter functionality

---
+++ Main.lua
@@ -0,0 +1,50 @@
+
+local hideCount = 0
+
+local BLACKLIST = {
+    "[bB][oO][oO][sS][tT]",
+}
+
+local SELL_PATTERNS = {
+    "[vV]erkauf",
+    "[wW][tT][sS]",
+    "[bB]iete[tn]",
+    "[vV][iI][pP]",
+    "[rR]abatt",
+}
+
+local SELL_TARGETS = {
+    "[mM]ythic",
+    "[mM]\+",
+    "[nN]'?[zZ]oth",
+    "[nN]y'?alotha",
+}
+
+local function filter_blacklist(text, blacklist)
+    for _, pattern in ipairs(blacklist) do
+        if text:find(pattern) then
+            return true
+        end
+    end
+    return false
+end
+
+local function filter_blacklist_pair(text, blacklist_1, blacklist_2)
+    return  filter_blacklist(text, blacklist_1) and
+            filter_blacklist(text, blacklist_2)
+end
+
+local function chat_filter(self, event, msg, author, ...)
+    local hide = false
+    if filter_blacklist(msg, BLACKLIST) or
+        filter_blacklist_pair(msg, SELL_PATTERNS, SELL_TARGETS) then
+        hideCount = hideCount + 1
+        hide = true
+    end
+    if hide == true and hideCount % 50 == 0 then
+        print("ChatFilter: Hidden " .. hideCount .. " messages")
+    end
+    return hide
+end
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", chat_filter)
