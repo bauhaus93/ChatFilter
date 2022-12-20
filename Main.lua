1db33ba938847c18208d2305f39f6b1581e4d69b
unknown <schlemihl123+git@gmail.com>
2022-12-20 19:16:28 +0100

    Some updates

--- Main.lua
+++ Main.lua
@@ -2,31 +2,36 @@
 local hideCount = 0
 
 local BLACKLIST = {
-    "[bB][oO][oO][sS][tT]",
-    --"[gG]arona"
+    "boost"
 }
 
 local SELL_PATTERNS = {
-    "[vV]erkauf",
-    "[kK]auf",
-    "[wW][tT][sS]",
-    "[bB]iete[tn]",
-    "[vV][iI][pP]",
-    "[rR]abatt",
-    "[sS]ale",
-    "[bB]ooking",
+    "kauf",
+    "wts",
+    "biete[tn]",
+    "vip",
+    "rabatt",
+    "sale",
+    "booking",
+    "boost",
+    "wow gold",
+    "nur für gold",
+    "[1-5][0-9][0-9]? ?k"
 }
 
 local SELL_TARGETS = {
-    "[mM]ythic",
-    "[mM]\+",
-    "[nN]'?[zZ]oth",
-    "[nN]y'?alotha",
+    "mythic",
+    "m\+",
+    "nathria",
+    "[kc]urve",
+    "torgh?ast",
+    "level up",
+    "levelup"
 }
 
 local function filter_blacklist(text, blacklist)
     for _, pattern in ipairs(blacklist) do
-        if text:find(pattern) then
+        if text:lower():find(pattern:lower()) then
             return true
         end
     end
@@ -39,16 +44,15 @@ local function filter_blacklist_pair(text, blacklist_1, blacklist_2)
 end
 
 local function chat_filter(self, event, msg, author, ...)
-    local hide = false
     if filter_blacklist(msg, BLACKLIST) or
         filter_blacklist_pair(msg, SELL_PATTERNS, SELL_TARGETS) then
         hideCount = hideCount + 1
-        hide = true
+        if hideCount % 250 == 0 then
+            print("ChatFilter: Hidden " .. hideCount .. " messages")
+        end
+        return true
     end
-    if hide == true and hideCount % 100 == 0 then
-        print("ChatFilter: Hidden " .. hideCount .. " messages")
-    end
-    return hide
+    return false
 end
 
 ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", chat_filter)
